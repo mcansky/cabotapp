6d4665a5443c1cad3f866222f1562fcdb6485583
Thomas Riboulet <thomas.riboulet@lyst.com>
2015-12-15 17:28:09 +0000

    fix listening IP, updating versions, adding whitenoise to handle assets

--- cabot/wsgi.py
+++ cabot/wsgi.py
@@ -1,5 +1,10 @@
 import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cabot.settings')
+
 import django.core.handlers.wsgi
+from whitenoise.django import DjangoWhiteNoise
+
 application = django.core.handlers.wsgi.WSGIHandler()
+application = DjangoWhiteNoise(application)
+
